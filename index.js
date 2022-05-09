f79c4d82aca55dc50ffc37ac15b85fbb197f6d79
systemilc <isaac.systemilc@gmail.com>
2022-05-09 20:11:23 -0300

    alterado variavel url

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var espacoPokemon = $('#poke-info')
+let espacoPokemon = $('#poke-info')
+
 
 $('#pesquisar').click(function(event){
     event.preventDefault();
 
-    var nome = $('#pk-name').val().toLowerCase();
-   
-    $.ajax({url: `https://pokeapi.co/api/v2/pokemon/${nome}`,
+    let nome = $('#pk-name').val().toLowerCase();
+    const url = "https://pokeapi.co/api/v2/pokemon/"
+    $.ajax({url: `${url}${nome}`,
          success: function (resultado){
             console.log(resultado)
-
+            
             $('#nomePokemon').html(`Nome: ${resultado.name}`)
-            var img = resultado.sprites.front_default;
+            let img = resultado.sprites.front_default;
             $('#fotoPokemon').attr('src', img)
             $('#codigoPokemon').html(`ID: ${resultado.id}`)
             $('#tipoPokemon').html(`Tipo: ${resultado.types[0].type.name}`)
